5f0581db73e6fd1f3360789fda6ecef6e35da75e
Rosana Peralta <rperalta@technisys.com>
2021-09-07 19:36:41 -0300

    file .env.example and modified readme

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,3 @@
-// import { createStore, applyMiddleware, compose } from 'redux';
-// import { createLogger } from 'redux-logger';
-// import thunkMiddleware from 'redux-thunk';
-// import reducer from './reducers';
-
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-// export default createStore(reducer, composeEnhancers(applyMiddleware(createLogger(), thunkMiddleware)));
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
