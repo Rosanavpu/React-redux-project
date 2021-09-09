8b82e17ae23a071ae1bd9c584b099303fde83c0d
Rosana Peralta <rperalta@technisys.com>
2021-09-09 17:18:25 -0300

    Showing the search input just in the pages of the videogames

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import VideoGames from './containers/videogames/Videogames';
 import DetailVideogame from './containers/detailVideogame/Detail';
+import SearchAppBar from './containers/navbar/Navbar';
 import PageNotFound from './components/pageNotFound/PageNotFound';
 
 function App() {
   return (
     <Router>
       <div>
+        <SearchAppBar search={false} />
         <Switch>
           <Route exact path='/' component={VideoGames} />
           <Route exact path='/videoGame/:id' component={DetailVideogame} />
-          <Route component={PageNotFound}/>
+          <Route component={PageNotFound} />
         </Switch>
       </div>
     </Router>
