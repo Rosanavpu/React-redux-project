2e8b149fab299b8ce98dce362b2a37367999ae4c
Rosana Peralta <rperalta@technisys.com>
2021-09-11 15:24:30 -0300

    style page not found

--- src/shared/pageNotFound/index.jsx
+++ src/shared/pageNotFound/index.jsx
@@ -1,13 +1,9 @@
-import { Container } from './styles';
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import { Typography } from '@material-ui/core';
+import { Container } from './styles';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   root: {
     width: '100%',
   },
@@ -22,27 +18,22 @@ const useStyles = makeStyles({
   pos: {
     marginBottom: 12,
   },
-});
+}));
 
 export default function PageNotFound() {
   const classes = useStyles();
-
   return (
-    <Container width={'75%'} justifyContent={'center'}>
-      <Card className={classes.root} variant='outlined'>
-        <CardContent>
-          <Container justifyContent={'center'} marginBottom={'2%'}>
-            <Typography variant='h5' component='h1'>
-             Ooops!
-            </Typography>
-          </Container>
-          <Container justifyContent={'center'}>
-            <Typography variant='body2' component='p'>
-              The link you clicked may be broken or the page may have been removed or renamed
-            </Typography>
-          </Container>
-        </CardContent>
-      </Card>
-    </Container>
+    <>
+      <Container padding={'1%'} justifyContent='center'>
+        <Typography variant='h4' gutterBottom>
+          The page you were looking for does not exist.
+        </Typography>
+      </Container>
+      <Container padding={'1%'} justifyContent='center'>
+        <Typography variant='h6'>
+          You may have mistyped the addres or the page may have moved
+        </Typography>
+      </Container>
+    </>
   );
 }
